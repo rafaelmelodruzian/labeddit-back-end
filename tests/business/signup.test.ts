378002eb7688eb25755936f747e51bc66c02118c
rafaelmelodruzian <rmdconsultor@gmail.com>
2023-09-06 14:10:22 -0300

    realizados testes na aplicação, foram testadas 75% das linhas das camadas e intencias importantes para aplicação, com uma taxa de 100% dos testes aprovados

---
+++ tests/business/signup.test.ts
@@ -0,0 +1,29 @@
+import { UserBusiness } from "../../src/business/UserBusiness";
+import { SignupSchema } from "../../src/dtos/user/signup.dto"
+import { HashManagerMock } from "../mocks/HashManagerMock";
+import { IdGeneratorMock } from "../mocks/IdGeneratorMock";
+import { TokenManagerMock } from "../mocks/TokenManagerMock";
+import { UserDatabaseMock } from "../mocks/UserDatabaseMock";
+
+describe("Testando signup", () => {
+  const userBusiness = new UserBusiness(
+    new UserDatabaseMock(),
+    new IdGeneratorMock(),
+    new TokenManagerMock(),
+    new HashManagerMock()
+  );
+
+  test("deve gerar token ao cadastrar", async () => {
+    const input = SignupSchema.parse({
+      name: "Ciclana",
+      email: "ciclana@email.com",
+      password: "ciclana321",
+    });
+
+    const output = await userBusiness.signup(input);
+
+    expect(output).toEqual({
+      token: "token-mock",
+    });
+  });
+});
---
+++ tests/mocks/UserDatabaseMock.ts
@@ -0,0 +1,56 @@
+import { USER_ROLES, UserDB } from "../../src/models/User";
+import { BaseDatabase } from "../../src/database/BaseDatabase";
+
+const usersMock: UserDB[] = [
+    {
+        id: "id-mock-fulano",
+        name: "Fulano",
+        email: "fulano@email.com",
+        password: "hash-mock-fulano", 
+        created_at: new Date().toISOString(),
+        role: USER_ROLES.NORMAL
+    },
+    {
+        id: "id-mock-astrodev",
+        name: "Astrodev",
+        email: "astrodev@email.com",
+        password: "hash-mock-astrodev", 
+        created_at: new Date().toISOString(),
+        role: USER_ROLES.ADMIN
+    },
+]
+
+export class UserDatabaseMock extends BaseDatabase {
+    public static TABLE_USERS = "users"
+
+    public async findUsers(
+        q: string | undefined
+    ): Promise<UserDB[]> {
+        if (q) {
+            return usersMock.filter(user =>
+                user.name.toLocaleLowerCase()
+                    .includes(q.toLocaleLowerCase()))
+
+        } else {
+            return usersMock
+        }
+    }
+
+    public async findUserById(
+        id: string
+    ): Promise<UserDB | undefined> {
+        return usersMock.filter(user => user.id === id)[0]
+    }
+
+    public async findUserByEmail(
+        email: string
+    ): Promise<UserDB | undefined> {
+        return usersMock.filter(user => user.email === email)[0]
+    }
+
+    public async insertUser(
+        newUserDB: UserDB
+    ): Promise<void> {
+
+    }
+}
